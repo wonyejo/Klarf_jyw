bec333e3cbf30517213b77bcbbc390b888eaec23
wonyejo <141125658+wonyejo@users.noreply.github.com>
2023-09-01 13:30:19 +0900

    add file
    add die-view-model file

---
+++ Klarf/ViewModel/DieViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Klarf.Model;
+
+namespace Klarf.ViewModel
+{
+    public class DieViewModel : ViewModelBase
+    {
+        #region[필드]
+        private Die die;
+        #endregion
+        #region[속성]
+       
+
+        public Die Die
+        {
+            get => die;
+            set
+            {
+                die = value;
+                OnPropertyChanged(nameof(Die));
+            }
+        }
+        public Tuple<int, int> GridCoordinate
+        {
+            get
+            {
+                if (Die != null)
+                {
+                    return Die.GridCoordinate;
+                }
+                return null;
+            }
+        }
+        #endregion
+
+
+
+
+
+    }
+}
